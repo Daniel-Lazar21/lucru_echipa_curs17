9af64927c716dd6c94f2d64ad5cf7f5f2f8673e8
Mihai Apostolescu <apostolescu.mihaicristian@yahoo.com>
2023-11-13 21:12:49 +0200

    Am adaugat functia nrTotalMasini

--- calcule.py
+++ calcule.py
@@ -1,1 +1,23 @@
 #calcule
+import os
+import csv
+#calculeaza numarul total de masini din tara
+FILE_PATH="/Users/mihaiapostolescu/Projects/Python/curs-17/"
+
+def read_csv(filename):
+    with open(FILE_PATH+filename,'r') as file:
+        reader = csv.reader(file)
+        returnable=list(reader)
+        return returnable
+
+def nrTotalMasini():
+    csvFile=read_csv("masini.csv")
+    count=0
+    nr_Total_Masini=0
+    for line in csvFile:
+        if count!=0:
+            nr_Total_Masini = nr_Total_Masini + int(line[7])
+        count+=1
+    print(nr_Total_Masini)
+
+nrTotalMasini()
