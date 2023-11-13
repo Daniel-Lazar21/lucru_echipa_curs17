0d23f92b4b3c8e82569af13a826d27e2f350121f
Krisztian Kovacs <kovacs.krisztian169@gmail.com>
2023-11-13 20:45:01 +0200

    Am facut o functie care schimba judetele  "dolj","olt","gorj","valcea","mehedinti" cu "OLTENIA"

--- modificari.py
+++ modificari.py
@@ -1,1 +1,10 @@
-# acesta este un test
+import pandas as pd
+CALE_FISIER = r"C:\Users\Krisztian\Probleme\Curs17\Proiect\masini.csv"
+
+def modificare_judet(cale_fisier):
+    df = pd.read_csv(cale_fisier)
+    df['JUDET'] = df["JUDET"].replace(['DOLJ', 'OLT','GORJ','VALCEA','MEHEDINTI'], 'OLTENIA')
+    df.to_csv(cale_fisier, index = False)
+    return df
+
+modificare_judet(CALE_FISIER)
